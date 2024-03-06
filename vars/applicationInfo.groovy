21221e28019ae1136e22858da33ac1f40ef26ed9
chandan-hn <121455237+chandan-hn@users.noreply.github.com>
2024-03-06 18:59:39 +0530

    Update applicationInfo.groovy

--- vars/applicationInfo.groovy
+++ vars/applicationInfo.groovy
@@ -1,10 +1,11 @@
 def call() {
-  script {
-  company_name: "Amazon"
-  url: "www.amazon.com"
-  ip_address: "192.168.29.193"
-    echo $company_name
-    echo $url
-    echo $ip_address
-  }
+    script {
+        def company_name = "Amazon"
+        def url = "www.amazon.com"
+        def ip_address = "192.168.29.193"
+        
+        echo company_name
+        echo url
+        echo ip_address
+    }
 }
